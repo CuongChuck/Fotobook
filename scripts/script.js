831b7498f0396ea587e99efd0bbaabf9fdceb37b
cuongchuck <englishtalent123@gmail.com>
2025-06-27 16:27:01 +0700

    Create login page and related components

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,7 @@ class Header extends HTMLElement {
                     <input class="rounded form-control-sm px-3 w-100" placeholder="Search Photo / Album"></input>
                 </div>
                 <div class="col offset-1 offset-md-2 col-2 d-flex flex-row justify-content-center">
-                    <img class="rounded-circle avatar" src="../res/penguin.png">
+                    <img class="rounded-circle avatar" src="../../res/penguin.png">
                     <h6 class="my-auto mx-3">User</h6>
                 </div>
                 <div class="col col-2">
@@ -25,4 +25,30 @@ class Header extends HTMLElement {
     }
 }
 
+class AuthorizeButton extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const isLogin = this.getAttribute("login") === "true";
+        this.innerHTML = `
+            <button class="authorize-button btn px-4 mt-3">${isLogin ? "Login" : "Signup"}</button>
+        `;
+    }
+}
+
 customElements.define('header-fotobook', Header);
+customElements.define('authorize-button', AuthorizeButton);
+
+googleicon = document.getElementById("google").addEventListener("click", () => {
+    window.open("https://accounts.google.com/signin", '_blank');
+});
+
+facebookicon = document.getElementById("facebook").addEventListener("click", () => {
+    window.open("https://facebook.com/login", '_blank');
+});
+
+xicon = document.getElementById("X").addEventListener("click", () => {
+    window.open("https://x.com/i/flow/login", '_blank');
+});
