8a318d577026eb1cb4563c2278dc37bc39de1c9a
cuongchuck <englishtalent123@gmail.com>
2025-07-17 09:49:51 +0700

    Initialize Rails project

---
+++ fotobook/app/assets/config/manifest.js
@@ -0,0 +1,4 @@
+//= link_directory ../stylesheets
+//= link application.css
+//= link_directory ../javascripts
+//= link_directory ../javascripts/controllers
---
+++ fotobook/config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  resources :users
+  resources :albums
+  resources :photos
+  resources :pictures
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  root "guest#photo"
+  get "/login", to: "guest#login"
+  get "/signup", to: "guest#signup"
+  get "/album", to: "guest#album"
+end
