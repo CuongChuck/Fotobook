cef9a302c0749b5f21a6a9852c29d3ef61c52f84
cuongchuck <englishtalent123@gmail.com>
2025-07-18 16:32:30 +0700

    Add show user profile

--- fotobook/app/controllers/users_controller.rb
+++ fotobook/app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
 
   # GET /users or /users.json
   def index
@@ -8,6 +7,9 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    if request.path.include?('photos')
+      @user = User.includes(:photos).where(id: current_user.id).first
+    end
   end
 
   # GET /users/new
@@ -58,11 +60,6 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params.expect(:id))
-    end
-
     # Only allow a list of trusted parameters through.
     def user_params
       params.fetch(:user, {})
