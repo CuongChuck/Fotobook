9d91a984d5c898abb90fd1a8aac0693e9743e267
cuongchuck <englishtalent123@gmail.com>
2025-07-18 13:52:49 +0700

    Add devise

--- fotobook/app/models/user.rb
+++ fotobook/app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable
+
     has_one :avatar, class_name: "Photo", foreign_key: "person_id"
 
-    has_and_belongs_to_many :followers, class_name: "User", join_table: "user1_follow_user2"
-    has_and_belongs_to_many :followees, class_name: "User", join_table: "user1_follow_user2"
+    has_and_belongs_to_many :followers, class_name: "User", join_table: "user1_follow_user2", foreign_key: "followee_id", association_foreign_key: "follower_id"
+    has_and_belongs_to_many :followees, class_name: "User", join_table: "user1_follow_user2", foreign_key: "follower_id", association_foreign_key: "followee_id"
 
     has_and_belongs_to_many :liked_photos, class_name: "Photo", join_table: "user_like_photo"
     has_and_belongs_to_many :liked_albums, class_name: "Album", join_table: "user_like_album"
@@ -13,5 +15,4 @@ class User < ApplicationRecord
     validates :fname, length: { maximum: 25 }, presence: true
     validates :lname, length: { maximum: 25 }, presence: true
     validates :email, length: { maximum: 255 }, presence: true, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP
-    validates :password, length: { maximum: 64 }, presence: true, confirmation: true
 end
