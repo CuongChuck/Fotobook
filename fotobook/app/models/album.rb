c9ca7e54006471807ac2a1b4ea6304df609c5427
cuongchuck <englishtalent123@gmail.com>
2025-07-28 15:59:05 +0700

    Add scopes

--- fotobook/app/models/album.rb
+++ fotobook/app/models/album.rb
@@ -1,6 +1,12 @@
 class Album < ApplicationRecord
-    has_and_belongs_to_many :photos, join_table: "album_has_photo"
+    default_scope { order(created_at: :desc) }
 
+    scope :include_photos, -> { includes(photos: [:image]) }
+    scope :include_likes, -> { includes(:user_like_photo) }
+    scope :public_only, -> { where(isPublic: true) }
+    scope :include_users, -> { includes(:user) }
+
+    has_and_belongs_to_many :photos, join_table: "album_has_photo"
     has_and_belongs_to_many :user_like_album, class_name: "User", join_table: "user_like_album"
 
     belongs_to :user
