068c349ceb4520168407bad28dc030f4e3905b71
cuongchuck <englishtalent123@gmail.com>
2025-07-28 15:44:58 +0700

    Add sidekiq queue adapter for active job

--- fotobook/config/application.rb
+++ fotobook/config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Fotobook
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
+    config.load_defaults 7.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -23,5 +23,6 @@ module Fotobook
     #
     config.time_zone = "Asia/Ho_Chi_Minh"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :sidekiq
   end
 end
