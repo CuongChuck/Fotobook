b8f6ab394d33537a85ffa147a85de46d244c54d6
cuongchuck <englishtalent123@gmail.com>
2025-07-30 09:17:49 +0700

    Use carrierwave

--- fotobook/db/migrate/20250718083454_create_photos.rb
+++ fotobook/db/migrate/20250718083454_create_photos.rb
@@ -3,10 +3,11 @@ class CreatePhotos < ActiveRecord::Migration[8.0]
     create_table :photos do |t|
       t.string :title
       t.text :description
-      t.string :url
+      t.string :image
       t.belongs_to :user, foreign_key: true
       t.belongs_to :person, foreign_key: { to_table: :users }
       t.boolean :isPublic
+      t.boolean :image_processing
       t.timestamps
     end
   end
